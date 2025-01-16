b85446e6ce35fe6a2be110fe7c16df723cbdee0a
caronf <florimond.caron@etu.umontpellier.fr>
2025-01-16 13:57:49 +0100

    WORKING VERSION (with-out swagger)

--- src/main/java/imt/production/dev/DevApplication.java
+++ src/main/java/imt/production/dev/DevApplication.java
@@ -3,10 +3,7 @@ package imt.production.dev;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
-
 @SpringBootApplication
-@EnableSwagger2
 public class DevApplication {
 
 	public static void main(String[] args) {
